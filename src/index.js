94229dbd3aba4493fe379baeac24acf510f7d483
Ryan Kustaborder <rkusty23@gmail.com>
2022-06-29 20:35:51 -0400

    removed app.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./import.js";
-import App from "./App";
+
+import KTopMenu from "./components/KTopMenu";
+
+import Home from "./pages/Home";
+import DesignGuidelines from "./pages/DesignGuidelines";
+import NavMenus from "./pages/NavMenus";
+import Scrollbars from "./pages/Scrollbars";
+import Carousels from "./pages/Carousels";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
-    <App />
+    <div className="app">
+      <KTopMenu
+        logo="./logo.svg"
+        index="0"
+        content={[
+          { text: "Guideline", href: "./guideline" },
+          {
+            isGroup: true,
+            text: "UI Components",
+            content: [
+              { text: "Nav Menus", href: "./navmenus" },
+              { text: "Scrollbars", href: "./scrollbars" },
+              { text: "Carousels", href: "./carousels" },
+            ],
+          },
+        ]}
+      />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/guideline" element={<DesignGuidelines />} />
+        <Route path="/navmenus" element={<NavMenus />} />
+        <Route path="/scrollbars" element={<Scrollbars />} />
+        <Route path="/carousels" element={<Carousels />} />
+      </Routes>
+    </div>
   </BrowserRouter>
 );
