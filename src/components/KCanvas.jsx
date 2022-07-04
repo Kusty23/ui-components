c4d16923849057544a2e25a1efadeb4ede407887
Ryan Kustaborder <rkusty23@gmail.com>
2022-07-04 15:56:09 -0400

    Tri Color Base Gradient works

--- src/components/KCanvas.jsx
+++ src/components/KCanvas.jsx
@@ -10,10 +10,10 @@ export default function KCanvas(props) {
   const tileWidth = 50;
 
   const keyColors = {
-    topLeft: { r: 255, g: 0, b: 0 },
-    botLeft: { r: 122, g: 0, b: 0 },
-    topRight: { r: 70, g: 0, b: 0 },
-    botRight: { r: 0, g: 10, b: 0 },
+    topLeft: { r: 255, g: 0, b: 140 },
+    botLeft: { r: 255, g: 190, b: 100 },
+    topRight: { r: 5, g: 45, b: 40 },
+    botRight: { r: 60, g: 255, b: 145 },
   };
 
   let gradient = Array(5);
@@ -48,32 +48,59 @@ export default function KCanvas(props) {
     // Top
     let rl = keyColors.topLeft.r;
     let rr = keyColors.topRight.r;
-
     let delr = (rr - rl) / (5 - 1); //one less than grid size
+
+    let gl = keyColors.topLeft.g;
+    let gr = keyColors.topRight.g;
+    let delg = (gr - gl) / (5 - 1); //one less than grid size
+
+    let bl = keyColors.topLeft.b;
+    let br = keyColors.topRight.b;
+    let delb = (br - bl) / (5 - 1); //one less than grid size
 
     for (let x = 0; x < 5; x++) {
       tiles[x].color.r = rl + delr * x;
+      tiles[x].color.g = gl + delg * x;
+      tiles[x].color.b = bl + delb * x;
     }
 
     // Bottom
     rl = keyColors.botLeft.r;
     rr = keyColors.botRight.r;
-
     delr = (rr - rl) / (5 - 1); //one less than grid size
+
+    gl = keyColors.botLeft.g;
+    gr = keyColors.botRight.g;
+    delg = (gr - gl) / (5 - 1); //one less than grid size
+
+    bl = keyColors.botLeft.b;
+    br = keyColors.botRight.b;
+    delb = (br - bl) / (5 - 1); //one less than grid size
 
     for (let x = 0; x < 5; x++) {
       tiles[20 + x].color.r = rl + delr * x;
+      tiles[20 + x].color.g = gl + delg * x;
+      tiles[20 + x].color.b = bl + delb * x;
     }
 
-    // 1st col
+    // Fill in the columns
     for (let col = 0; col < 5; col++) {
       let rt = tiles[col].color.r;
       let rb = tiles[20 + col].color.r;
-
       delr = (rb - rt) / (5 - 1);
+
+      let gt = tiles[col].color.g;
+      let gb = tiles[20 + col].color.g;
+      delg = (gb - gt) / (5 - 1);
+
+      let bt = tiles[col].color.b;
+      let bb = tiles[20 + col].color.b;
+      delb = (bb - bt) / (5 - 1);
 
       for (let x = 0; x < 5; x++) {
         tiles[x * 5 + col].color.r = rt + delr * x;
+        tiles[x * 5 + col].color.g = gt + delg * x;
+        tiles[x * 5 + col].color.b = bt + delb * x;
       }
     }
   };
